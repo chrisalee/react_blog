82eeeeff25d56b4b87a33c5e467c847fd40d750c
Chris Lee <christopher.lee222@yahoo.com>
2022-05-07 23:56:54 -0500

    login page

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import Navbar from './components/navbar/Navbar';
 // import Home from './pages/home/Home';
 // import Single from './pages/single/Single';
 // import Write from './pages/write/Write';
-import Settings from './pages/settings/Settings';
+// import Settings from './pages/settings/Settings';
+import Login from './pages/login/Login';
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
-      <Settings />
+      <Login />
     </div>
   )
 }
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,11 +1,19 @@
-import './login.css';
+import "./login.css";
 
 const Login = () => {
   return (
-    <div className='login'>
-        login
+    <div className="login">
+      <span className="login__title">Login</span>
+      <form className="login__form">
+        <label>Email</label>
+        <input type="email" className="login__input" placeholder="Enter Email" />
+        <label>Password</label>
+        <input type="password" className="login__input" placeholder="Enter Password" />
+        <button className="login__button">Login</button>
+      </form>
+      <button className="login__registerButton">Not a member, Register here</button>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
