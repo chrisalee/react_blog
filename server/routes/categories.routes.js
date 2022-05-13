9beabaa86da173c0666377ab684f4fc23ad63476
Chris Lee <christopher.lee222@yahoo.com>
2022-05-12 23:02:20 -0500

    categories.routes updated

--- server/routes/categories.routes.js
+++ server/routes/categories.routes.js
@@ -5,7 +5,7 @@ const Category = require('../models/category.model');
 router.post("/", async (request, response) => {
   const newCategory = new Category(request.body);
   try {
-    const savedCategory = newCategory.save();
+    const savedCategory = await newCategory.save();
     response.status(200).json(savedCategory);
   } catch (error) {
     response.status(500).json(error);
